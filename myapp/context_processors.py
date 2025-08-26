f0e5862aba83835ac3c3c7fb5be0cedb651b7b66
lindeman <lindemanguerra@gmail.com>
2025-08-26 00:30:35 -0500

    Categorias OK

--- myapp/context_processors.py
+++ myapp/context_processors.py
@@ -2,13 +2,10 @@
 
 from .models import Categoria
 
-"""def categorias_context(request):
-    return {
-        "categorias_menu": Categoria.objects.all()
-    }
-"""
+
 
 def categorias_context(request):
-    categorias = Categoria.objects.all()
-    print("🔎 Categorías cargadas en el context_processor:", categorias)  # 👈 depuración
-    return {"categorias_menu": categorias}
+    return {
+        "categorias_menu": Categoria.objects.all().order_by("nombre")
+    }
+
